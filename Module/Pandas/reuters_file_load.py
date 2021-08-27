d67d39eee1889912c80d346f139c6d073a56064f
Kulamani Pradhan <kulamanipradhan0@gmail.com>
2021-08-27 23:15:26 +0200

    data frame

--- Module/Pandas/reuters_file_load.py
+++ Module/Pandas/reuters_file_load.py
@@ -2,21 +2,21 @@ import os,datetime,psycopg2 as pg
 import pandas as pd
 import dask.dataframe as dd
 
-os.chdir("H:\Sprints\mifid\Temporary\BANCS_Identification")
-filename='IDENTIFICATION_20210309.csv'
+os.chdir("")
+filename='abc.csv'
 p_bsdate=20210109
 p_bccentity='FCA'
 seqnum=1
 
 
 print('Started : ',datetime.datetime.now())
-sitrds = pg.connect(host='10.248.150.213',
-                    port='5444',
-                    database='i200gut1',
-                    user='grip_own',
-                    password='changeme')
+sitrds = pg.connect(host='localhost' ,
+                    port='5432',
+                    database='postgres',
+                    user='postgres',
+                    password='password')
 cursor = sitrds.cursor()
-print("You are connected to SIT RDS atabase(User : grip_own)")
+print("You are connected to database (postgres)")
 
 header_col=list(pd.read_csv(filename, nrows=1, sep='|').columns)
 print(header_col)
@@ -25,7 +25,6 @@ df=pd.DataFrame(df)
 #df=df.fillna('')
 print(df.columns)
 
-df=pd.read_csv('H:\Sprints\mifid\Temporary\BANCS_Identification\IDENTIFICATION_20210309.csv',sep='|',header=1,quotechar='"',chunksize=10000)
+df=pd.read_csv('abc.csv',sep='|',header=1,quotechar='"',chunksize=10000)
 
 #print(df.info())
-#print(df[['INSTRMNT_ID', 'OLD_SCRTY_ID', 'RANK', 'SEC_ID_SCHM', 'SEC_ID_SCHM_CD']])
